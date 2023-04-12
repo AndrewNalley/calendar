f0deaa8f7fcb2c313a01530cbb00708879b20a47
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-04-11 19:46:16 -0500

    add hours for loop

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
 $(function () {
-    // TODO: Add a listener for click events on the save button.
+    
 $("button").click(function() {
   var userInput = $(this).closest(".time-block").attr("id");
-  //This code should use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  
 
   var key = "mykey-" + userInput.id;
   var value = JSON.stringify()// text area content);
   localStorage.setItem(key, value);
+  console.log(userInput);
 });
+var hourArray = ["#hour-8", "#hour-9", "#hour-10", "#hour-11", "#hour-12", "#hour-13", "#hour-14", "#hour-15", "#hour-16", "#hour-17", "#hour-18", "#hour-19"];
+var now = dayjs();
+
+for (var i = 0; i < hourArray.length; i++) {
+  var hour = $(hourArray[i]);
+  if (hour > now) {
+    $(this).closest("div").addClass("future")
+  } else if (hour < now) {
+    $(this).closest("div").addClass("past")
+  } else {
+    $(this).closest("div").addClass("present")
+  }
+}
+console.log(now)
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
